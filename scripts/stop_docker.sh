3f30e5300bd0c5e329699a845aa448910a39685b
laddu344 <valmiki.9398@gmail.com>
2025-10-27 19:18:08 +0530

    Update stop_docker.sh

--- scripts/stop_docker.sh
+++ scripts/stop_docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+echo "Stopping existing Docker containers..."
+
+# Stop running containers (if any)
+sudo docker ps -q | xargs -r sudo docker stop
+
+# Optional: remove old containers
+sudo docker system prune -f
+
+echo "Stopped old containers successfully."
