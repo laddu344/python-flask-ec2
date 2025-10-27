4725110322de360b5f6351a2067690463a6f0a0a
laddu344 <valmiki.9398@gmail.com>
2025-10-27 19:20:49 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "Installing Docker if not installed..."
+set -e
+
+echo "Installing dependencies..."
 sudo yum update -y
 sudo yum install -y docker
-sudo systemctl start docker
+
 sudo systemctl enable docker
+sudo systemctl start docker
+
+echo "Dependencies installed and Docker started."
